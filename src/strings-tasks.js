aa1c9dd56018e1f9661411a113a179a8f632a076
Tatyana Dmitrieva <visyar76@gmail.com>
2023-11-17 14:26:13 +0300

    feat: add function repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -141,8 +141,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (str === '' || times < 0) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
